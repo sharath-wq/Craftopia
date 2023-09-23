11c55b00b62f2325f44bfa1e6df58759dc97d5a0
sharath-wq <chandranpsharath@gmail.com>
2023-09-24 00:26:37 +0530

    Order Completed

--- controllers/admin/orderController.js
+++ controllers/admin/orderController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const Order = require("../../models/orderModel");
 
 /**
  * Manage Orders Page Route
@@ -6,7 +7,23 @@ const asyncHandler = require("express-async-handler");
  */
 exports.ordersPage = asyncHandler(async (req, res) => {
     try {
-        res.render("admin/pages/order/orders", { title: "Orders" });
+        const orderItems = await Order.find()
+            .select("orderNumber createdAt products.product status")
+            .populate({
+                path: "products.product",
+                model: "Product",
+                populate: {
+                    path: "images",
+                    model: "Images",
+                },
+            })
+            .populate({
+                path: "address",
+                model: "Address",
+            })
+            .sort({ createdAt: -1 });
+        // res.json(orderItems);
+        res.render("admin/pages/order/orders", { title: "Orders", orderItems });
     } catch (error) {
         throw new Error(error);
     }
@@ -18,7 +35,44 @@ exports.ordersPage = asyncHandler(async (req, res) => {
  */
 exports.editOrder = asyncHandler(async (req, res) => {
     try {
-        res.render("admin/pages/order/edit-order", { title: "Edit Order" });
+        const orderId = req.params.id;
+        const order = await Order.find({ orderNumber: orderId })
+            .select("orderNumber createdAt products.product products.quantity totalAmount paymentMethod status")
+            .populate({
+                path: "products.product",
+                model: "Product",
+                populate: {
+                    path: "images",
+                    model: "Images",
+                },
+            })
+            .populate({
+                path: "address",
+                model: "Address",
+            })
+            .sort({ createdAt: -1 });
+        // res.json(order);
+        res.render("admin/pages/order/edit-order", { title: "Edit Order", order });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+/**
+ * Update Order Status
+ * Method PUT
+ */
+exports.updateOrderStatus = asyncHandler(async (req, res) => {
+    try {
+        const orderId = req.params.id;
+        // res.json(req.body);
+        const order = await Order.findOneAndUpdate(
+            { orderNumber: orderId },
+            {
+                status: req.body.status,
+            }
+        );
+        res.redirect("/admin/orders");
     } catch (error) {
         throw new Error(error);
     }
